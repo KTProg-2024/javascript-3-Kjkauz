85c6bfdad676c9f36333afa2e762404a83bc66fa
Kevin Kauzens <>
2021-05-28 14:00:39 +0000

    added moving blue box

--- motion.js
+++ motion.js
@@ -2,7 +2,8 @@ function myMove() {
   var elem = document.getElementById("animate");   
   var posTop = 0;
   var posLeft = 0;
-  var id = setInterval(frame, 5);
+  var id = setInterval(frame, 5
+  );
   function frame() {
 	  posTop++;
 	  posLeft++;
@@ -10,5 +11,19 @@ function myMove() {
 	  elem.style.left = posLeft + "px"; 
 	  if ((posTop == 350) && (posLeft == 350))
 		  clearInterval(id);
+  }
+}
+function myMove2() {
+  var elem = document.getElementById("animate2");
+  var posTop = 47;
+  var posLeft = 358;
+  var id = setInterval(frame, 5);
+  function frame() {
+    posTop++;
+    posLeft--;
+    elem.style.top = posTop + "px";
+    elem.style.left = posLeft + "px";
+    if ((posTop > 350) && (posLeft < 0))
+      clearInterval(id);
   }
 }
